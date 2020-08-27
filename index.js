ce2ac4dfa93220aaae74e0d9b88617732117bba6
affaf <affaf.rai@hotmail.com>
2020-08-27 23:27:42 +0000

    submitting fetchCoordsByIP

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ fetchMyIP((error, ip) => {
   console.log('It worked! Returned IP:' , ip);
 });
 
-fetchCoordsByIP( "24.150.23.167", (error , data) => {
-  console.log(error + "   " + data);
-} )
+fetchCoordsByIP('162.245.144.188', (error, coords) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned Coords:' , coords);
+});
--- iss.js
+++ iss.js
@@ -1,5 +1,6 @@
 //iss.js
 const request = require('request');
+
 const fetchMyIP = function(callback) { 
   request('https://api.ipify.org?format=json', (error, response, body) => {
     if (error) {
@@ -17,7 +18,23 @@ const fetchMyIP = function(callback) {
 }
 
 const fetchCoordsByIP = function(ip, callback) {
+  request(`https://ipvigilante.com/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error,null);
+      return;
+    }
 
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const latitudeNum = JSON.parse(body).data.latitude;
+    const longitudeNum = JSON.parse(body).data.longitude;
+    const latLng = {latitude: latitudeNum, longitude: longitudeNum}
+   
+    callback(null, latLng);
+  });
 }
 
 
